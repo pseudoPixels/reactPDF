352d99c5a38cfdf04d24e752fa001de711d5ec6f
Golam Mostaeen <Golam.Mostaeen@glu.com>
2023-07-13 21:49:40 -0400

    show thumbnails

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import samplePDF from "./test.pdf";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  useEffect(() => {
+    pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  });
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function handleThumbnailClick(page) {
+    setPageNumber(page);
+  }
+
+  function renderThumbnails() {
+    const thumbnails = [];
+    for (let i = 1; i <= numPages; i++) {
+      thumbnails.push(
+        <div
+          key={`thumbnail-${i}`}
+          className={`thumbnail ${i === pageNumber ? "active" : ""}`}
+          onClick={() => handleThumbnailClick(i)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Document file={samplePDF}>
+            <Page
+              key={`thumbnail-page-${i}`}
+              pageNumber={i}
+              width={80}
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+            />
+          </Document>
+        </div>
+      );
+    }
+    return thumbnails;
+  }
+
+  return (
+    <>
+      <div className="pdf-viewer">
+        <div className="thumbnails">{renderThumbnails()}</div>
+        <div className="document">
+          <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} width={800} />
+          </Document>
+        </div>
+      </div>
+      <div>
+        <p>
+          Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+        </p>
+        <button type="button" disabled={pageNumber <= 1} onClick={() => changePage(-1)}>
+          Previous
+        </button>
+        <button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={() => changePage(1)}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 }
 
